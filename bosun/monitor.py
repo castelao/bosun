de781cd5a788d32cab69401a5c57365a55abe85c
Gui <guilherme@castelao.net>
2015-08-21 09:59:55 -0300

    qstat returns None if there is no such Job ID.

--- bosun/monitor.py
+++ bosun/monitor.py
@@ -5,16 +5,18 @@ import os
 def qstat(job_id):
     data = os.popen("qstat -a %s" % job_id).read()
 
+    if data == '':
+        return
+
     statuses = {}
-    if data != '':
-        header = None
-        for line in data.split('\n'):
-            if header and not line.startswith('----'):
-                info = line.split()
-                try:
-                    statuses[info[0]] = dict(zip(header.split()[1:], info))
-                except IndexError:
-                    pass
-            elif line.startswith('Job ID'):
-                header = line
+    header = None
+    for line in data.split('\n'):
+        if header and not line.startswith('----'):
+            info = line.split()
+            try:
+                statuses[info[0]] = dict(zip(header.split()[1:], info))
+            except IndexError:
+                pass
+        elif line.startswith('Job ID'):
+            header = line
     return statuses
